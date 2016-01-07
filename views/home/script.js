4223b3ffd2199893f72c6eb586beac4cb0ff67a6
Adam88Stanley <adam.stanulewicz@wp.pl>
2016-01-07 15:24:12 +0100

    Revert "update"
    This reverts commit 63e105bf2f5ca3a78e8c0c8d6755945a2089e817.

--- views/home/script.js
+++
@@ -1,5 +0,0 @@
-$("quick_view").click(function(){
-    $.ajax({url: "demo_test.txt", success: function(result){
-        $("#div1").html(result);
-    }});
-});
--- views/success/index.php
+++
@@ -1,7 +0,0 @@
-<?php use Lib\View; ?>
-<?php View::header('<link rel="stylesheet" type="text/css" href="'.URL.'views/success/content.css" />'); 
- ?>
-<div class="success">
-	<h3><?php echo $this->args['success'];?>.</h3>
-</div>
-<?php View::footer(); ?>
